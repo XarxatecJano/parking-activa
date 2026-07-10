6bba9c623697b2167f47bc5afc4fd818657058e8
Jano Soler <janosolerdiaz@gmail.com>
2026-07-10 10:10:04 +0200

    feat: new class Stay

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 // Vehicle, OfficialVehicle, ResidentVehicle, NonResidentVehicle
 // Parking, Stay
 
+class Stay {
+    #vehicle: Vehicle;
+    #entryDateTime: Date;
+    #exitDateTime?: Date;
+
+    constructor(vehicle: Vehicle){
+        this.#vehicle = vehicle;
+        this.#entryDateTime = new Date();
+    }
+
+    get vehicle() {
+        return this.#vehicle;
+    }
+
+    get entryDateTime() {
+        return this.#entryDateTime;
+    }
+
+    get exitDateTime():Date|undefined {
+        return this.#exitDateTime;
+    }
+
+    set exitDateTime(exitTime: Date){
+        this.#exitDateTime = exitTime;
+    }
+}
+
 class Vehicle {
 
     #plate: string;
@@ -30,10 +57,14 @@ class ResidentVehicle extends Vehicle{
     addTimeOfCurrentStay(minutes:number):void{
         this.#parkedTimeCurrentMonth += minutes;
     }
+
+    calculateStayMinutes(stay:Stay):number {
+        return stay.exitDateTime!.getTime() - stay.entryDateTime.getTime();
+    }
        
-    manageExit():void{//le paso como parametro una estancia
-        //añadir los minutos
-        //registrar hora de salida en las estancia
+    manageExit(stay:Stay):void{
+        stay.exitDateTime = new Date();//añadir los minutos
+        this.addTimeOfCurrentStay(this.calculateStayMinutes(stay));//registrar hora de salida en las estancia
     }
 
 }
